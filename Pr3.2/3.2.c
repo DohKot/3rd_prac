25bd3234b92f2f27b8b8a22d359b6f8b262a3226
sa <sa@astra>
2025-03-13 18:45:20 +0300

    second commit

--- Pr3.2/3.2.c
+++ Pr3.2/3.2.c
@@ -8,9 +8,6 @@ int main()
   ptr1=(float*)malloc(sizeof(float));
   ptr2=(float*)malloc(sizeof(float));
   ptr3=(float*)malloc(sizeof(float));
-  printf("%d \n",ptr1);
-  printf("%d \n",ptr2);
-  printf("%d \n",ptr3);
   *ptr1=256.66;
   *ptr2=0.78;
   *ptr3=45.55;
@@ -27,4 +24,3 @@ int main()
   free(ptr2);
   free(ptr3);
 }
-
--- Pr3.4/3.4.c
+++ Pr3.4/3.4.c
@@ -1,58 +1,61 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include <math.h>
-void massive(float *h, int n)
+void massive(float* h, int n)
 {
-    int count=0;
-    for (int j=0; j<n; j++)
+    int count = 0;
+    for (int j = 0; j < n; j++)
     {
-        if(h[j]>=0)
+        if (h[j] >= 0)
             count++;
     }
-    float *c=malloc(count*sizeof(float));
-    int count_c=0;
-    for (int i=0; i<n; i++)
+    float* c = (float*) malloc(count * sizeof(float));
+    int count_c = 0;
+    for (int i = 0; i < n; i++)
     {
-        if(h[i]>=0)
+        if (h[i] >= 0)
         {
-            c[count_c]=(pow(h[i], 0.5)/5);
+            c[count_c] = (pow(h[i], 0.5) / 5);
             count_c++;
         }
     }
-    for(int i=0; i<count_c; i++)
+    for (int i = 0; i < count; i++)
         printf("%f\n", c[i]);
+
+    int minIdx;
+    float temp;
+    for (int i = 0; i < count - 1; i++) {
+        minIdx = i;
+        for (int j = i + 1; j < count; j++) {
+            if (c[j] < c[minIdx]) {
+                minIdx = j;
+            }
+        }
+        // Меняем местами найденный минимальный элемент с текущим элементом
+        temp = c[minIdx];
+        c[minIdx] = c[i];
+        c[i] = temp;
+    }
+
+    // Вывод отсортированного массива
+    printf("Отсортированный массив:\n");
+    for (int i = 0; i < count; i++) {
+        printf("%f\n", c[i]);
+    }
+    free(c);
 
 }
 int main()
 {
-    int n=5;
-    int count=0;
-    float *b=malloc(n*sizeof(float));
-    b[0]=-23;
-    b[1]=12;
-    b[2]=34;
-    b[3]=98.78;
-    b[4]=-123.456;
+    int n = 6;
+    int count = 0;
+    float* b = (float*) malloc(n * sizeof(float));
+    b[0] = -23;
+    b[1] = 12;
+    b[2] = 34;
+    b[3] = 98.78;
+    b[4] = -123.456;
+    b[5] = 0.899;
     massive(b, n);
-    //printf("%f\n", c);
-    /*for (int i=0; i<n; i++)
-        printf("%f\n", b[i]);
-    for (int j=0; j<n; j++)
-    {
-        if(b[j]>=0)
-            count++;
-    }
-    float *c=malloc(count*sizeof(float));
-    int count_c=0;
-    for (int i=0; i<n; i++)
-    {
-        if(b[i]>=0)
-        {
-            c[count_c]=(pow(b[i], 0.5)/5);
-            count_c++;
-        }
-    }
-    for(int i=0; i<count_c; i++)
-        printf("%f\n", c[i]);*/
+    free(b);
 }
-
